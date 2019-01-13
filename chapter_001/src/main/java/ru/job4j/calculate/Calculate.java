b4bf7065b49bbb851130779cc05b6448f6554409
Evgeny Novoselov <zkodru@gmail.com>
2019-01-13 22:03:58 +0300

    1.3. JavaDoc. Code convention[#104804]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate now have only main() method
+ *
+ * @author Evgeny Novoselov
+ */
 public class Calculate {
+
+	/**
+	 * Method only println "Hello World" in console
+	 * 
+	 * @param args Array string arguments
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ * 
+ * @author Evgeny Novoselov
+ * @version $id$
+ * @since 0.1
+ */
+package ru.job4j.calculate;
