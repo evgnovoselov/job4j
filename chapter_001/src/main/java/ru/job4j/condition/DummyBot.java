346bcde28424ec59a94546716ce2f0ff36e7ac4d
Evgeny Novoselov <zkodru@gmail.com>
2019-01-19 22:28:28 +0300

    Beautiful code in DummyBot

--- chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -14,15 +14,10 @@ public class DummyBot {
      */
     public String answer(String question) {
         String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
-
         if ("Привет, Бот.".equals(question)) {
-
             rsl = "Привет, умник.";
-
         } else if ("Пока.".equals(question)) {
-
             rsl = "До скорой встречи.";
-            
         }
         return rsl;
     }
