90aeaeab8d0d98edc50da49fa8f1d8441fe46c3f
Evgeny Novoselov <zkodru@gmail.com>
2019-01-27 23:47:16 +0300

    Поправил метод проверки на выигрыш

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -69,9 +69,9 @@ public class Logic {
     public boolean isWin() {
         int[][] table = this.convert();
         boolean result = false;
-        int horizontalWin = 0;
-        int verticalWin = 0;
         for (int i = 0; i < table.length; i++) {
+            int horizontalWin = 0;
+            int verticalWin = 0;
             for (int j = 0; j < table.length; j++) {
                 if (table[i][j] == 1) {
                     horizontalWin++;
@@ -83,9 +83,6 @@ public class Logic {
             if (verticalWin == table.length || horizontalWin == table.length) {
                 result = true;
                 break;
-            } else {
-                verticalWin = 0;
-                horizontalWin = 0;
             }
         }
         return result;
