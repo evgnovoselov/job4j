1bb0df36c6371370cad99ace63a51828786f7eaf
Evgeniy Novoselov <zkodru@gmail.com>
2019-05-24 12:09:10 +0300

    Кофемашина. [#34741]
    Мелкое исправление теста кофемашины
    
    Исправил код кофемашины

---
+++ chapter_002/src/main/java/ru/job4j/coffeemachine/Machine.java
@@ -0,0 +1,46 @@
+package ru.job4j.coffeemachine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Реализация метода выдачи сдачи для автомата.
+ *
+ * @author Evgeny Novoselov
+ */
+public class Machine {
+    /**
+     * Монеты в автомате.
+     */
+    private static int[] coins = new int[]{10, 5, 2, 1};
+
+    /**
+     * Метод выдачи сдачи для автомата.
+     *
+     * @param value внесли денег. 50 100 и тд.
+     * @param price цена кофе.
+     * @return массив монет.
+     */
+    public static int[] changes(int value, int price) throws NeedMoreMoneyException {
+        if (value - price < 0) {
+            throw new NeedMoreMoneyException();
+        }
+        int change = value - price;
+        List<Integer> changeCoins = new ArrayList<>();
+        while (change > 0) {
+            for (int coin : coins) {
+                if (change - coin >= 0) {
+                    change -= coin;
+                    changeCoins.add(coin);
+                    break;
+                }
+            }
+        }
+        int[] changes = new int[changeCoins.size()];
+        for (int i = 0; i < changeCoins.size(); i++) {
+            changes[i] = changeCoins.get(i);
+        }
+        return changes;
+    }
+
+}
---
+++ chapter_002/src/test/java/ru/job4j/coffemachine/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Тестирование кофемашины.
+ *
+ * @author Evgeny Novoselov
+ */
+package ru.job4j.coffemachine;
