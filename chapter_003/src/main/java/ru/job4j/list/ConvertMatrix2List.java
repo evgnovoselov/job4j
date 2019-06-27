1d18572e38d9cf4f7fed432acbbde76f3c13c4b4
Evgeny Novoselov <zkodru@gmail.com>
2019-06-27 19:45:10 +0300

    Реализовал метод конвертации двумерного массива в коллекцию List

--- chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -17,6 +17,11 @@ public class ConvertMatrix2List {
      */
     public List<Integer> toList(int[][] array) {
         List<Integer> list = new ArrayList<>();
+        for (int[] items : array) {
+            for (int item : items) {
+                list.add(item);
+            }
+        }
         return list;
     }
 }
