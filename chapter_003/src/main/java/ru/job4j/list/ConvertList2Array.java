f5182c36f295f8c0e76e6c6bbd57e9ceda3bb3ef
Evgeny Novoselov <zkodru@gmail.com>
2019-06-27 15:00:39 +0300

    Добавил класс для конвертации коллекции в массив

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -0,0 +1,22 @@
+package ru.job4j.list;
+
+import java.util.List;
+
+/**
+ * Класс конвертации List в Array.
+ *
+ * @author Evgeny Novoselov
+ */
+public class ConvertList2Array {
+    /**
+     * Метод конвертации колекции List в двуменрный массив.
+     *
+     * @param list коллекция List.
+     * @param rows количество строк.
+     * @return двумерный массив равномерно разбимтый на количество строк.
+     */
+    public int[][] toArray(List<Integer> list, int rows) {
+        // TODO result massive.
+        return new int[0][0];
+    }
+}
