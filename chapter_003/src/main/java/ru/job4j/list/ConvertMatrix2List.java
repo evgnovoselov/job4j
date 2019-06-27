c799d5e55c46a0a222fe00bddef805ba6f0240ee
Evgeny Novoselov <zkodru@gmail.com>
2019-06-27 17:47:41 +0300

    Добавил класс конвертации матрицы в коллекцию и его тест

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -0,0 +1,22 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс конвертации двумерного массива в коллекцию List.
+ *
+ * @author Evgeny Novoselov
+ */
+public class ConvertMatrix2List {
+    /**
+     * Метод конвертации двумерного массива в коллекцию List.
+     *
+     * @param array массив.
+     * @return коллекция List.
+     */
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+        return list;
+    }
+}
