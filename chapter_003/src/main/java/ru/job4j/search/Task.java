d5b4fbc8dbffc9cae91a652ee8818c243232ad55
Evgeny Novoselov <zkodru@gmail.com>
2019-06-27 14:26:43 +0300

    Добавил описание к конструктору задач

--- chapter_003/src/main/java/ru/job4j/search/Task.java
+++ chapter_003/src/main/java/ru/job4j/search/Task.java
@@ -9,6 +9,12 @@ public class Task {
     private String desc;
     private int priority;
 
+    /**
+     * Конструктор задачи.
+     *
+     * @param desc     описание задачи.
+     * @param priority приоритет задачи.
+     */
     public Task(String desc, int priority) {
         this.desc = desc;
         this.priority = priority;
