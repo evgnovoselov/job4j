8bb9b42aa3f50da4256514fdbafa4dde931dd26a
Evgeny Novoselov <zkodru@gmail.com>
2019-06-28 21:18:54 +0300

    4. Конвертация двумерного массива в ArrayList[#104829] (#5)
    * Добавил класс конвертации матрицы в коллекцию и его тест
    
    * Реализовал метод конвертации двумерного массива в коллекцию List

---
+++ chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тестовый класс конвертации двумерного массива в коллекцию List.
+ *
+ * @author Evgeny Novoselov
+ */
+public class ConvertMatrix2ListTest {
+    /**
+     * Проверка метода конвертации мартицы в коллекцию List.
+     */
+    @Test
+    public void when2on2ArrayThenList4() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+}
