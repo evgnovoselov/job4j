ccdc0050158d4817053b6cb56fb039fc78c12c64
Evgeny Novoselov <zkodru@gmail.com>
2019-07-06 14:35:20 +0300

    Добавил шаблоны задач

---
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+/**
+ * Класс для расчета сколько месяцев Ивану нужно тренироваться, чтобы обогнать Николая в тяге.
+ * Иван за месяц увеличивает силу тяги в 3 раза за счет диеты, а Николай - за месяц увеличивает силу тяги в 2 раза.
+ *
+ * @author Evgeny Novoselov
+ */
+public class Fitness {
+    public int calc(int ivan, int nik) {
+        int month = 0;
+        return month;
+    }
+}
