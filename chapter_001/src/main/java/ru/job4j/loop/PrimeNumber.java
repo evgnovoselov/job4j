d443ae6bb02a8e0ba5e6231fd1e9e7a3509de251
Evgeniy Novoselov <zkodru@gmail.com>
2019-07-25 13:05:43 +0300

    Добавил необходимые файлы по заданию и комментарии к классам

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+/**
+ * Класс считает количество простых чисел от 1 до x.
+ *
+ * @author Evgeny Novoselov
+ */
+public class PrimeNumber {
+    public int calc(int finish) {
+        int count = 0;
+        return count;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Тестирование класса нахождения количества простых чисел от 1 до x
+ *
+ * @author Evgeny Novoselov
+ */
+public class PrimeNumberTest {
+    /**
+     * Тестирование нахождения количество простых чисел с 1 до 5.
+     */
+    @Test
+    public void when5() {
+        PrimeNumber prime = new PrimeNumber();
+        int count = prime.calc(5);
+        assertThat(count, is(3));
+    }
+
+    /**
+     * Тестирование нахождения количество простых чисел с 1 до 11.
+     */
+    @Test
+    public void when11() {
+        PrimeNumber prime = new PrimeNumber();
+        int count = prime.calc(11);
+        assertThat(count, is(5));
+    }
+
+    /**
+     * Тестирование нахождения количество простых чисел с 1 до 2.
+     */
+    @Test
+    public void when1() {
+        PrimeNumber prime = new PrimeNumber();
+        int count = prime.calc(2);
+        assertThat(count, is(1));
+    }
+}
