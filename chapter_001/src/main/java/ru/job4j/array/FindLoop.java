4205efc61425a6efa54cda9f64c263aee38b6b19
Evgeniy Novoselov <zkodru@gmail.com>
2019-07-31 12:31:18 +0300

    Подготовил к решению

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -24,4 +24,19 @@ public class FindLoop {
         }
         return rst;
     }
+
+    /**
+     * Метод поиска элемента в массиве по указанному диапозону.
+     * Если элемента нет в массиве, то возвращаем -1.
+     *
+     * @param data   Массив чисел.
+     * @param el     Элемент, который нужно найти.
+     * @param start  Индекс, с которого начинаем поиск.
+     * @param finish Индекс, которым заканчиваем поиск.
+     * @return Возвращаем индекс найденного элемента, в противном случае -1.
+     */
+    public int indexOf(int[] data, int el, int start, int finish) {
+        int result = -1;
+        return result;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -36,4 +36,32 @@ public class FindLoopTest {
         int expect = -1;
         assertThat(result, is(expect));
     }
+
+    /**
+     * TODO Add Comments
+     */
+    @Test
+    public void whenArrayHasLength5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * TODO Add Comments
+     */
+    @Test
+    public void whenFind3() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = find.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
 }
