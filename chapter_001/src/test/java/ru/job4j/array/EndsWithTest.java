66f1669a087b7246f6a63921048359f723c90d13
Evgeniy Novoselov <zkodru@gmail.com>
2019-07-31 12:15:30 +0300

    реализовал метод проверки постфикса

--- chapter_001/src/test/java/ru/job4j/array/EndsWithTest.java
+++ chapter_001/src/test/java/ru/job4j/array/EndsWithTest.java
@@ -6,28 +6,38 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
- * TODO add comments
+ * Тестирование метода по проверке постфикса.
  *
  * @author Evgeny Novoselov
  */
 public class EndsWithTest {
     /**
-     * TODO add comments
+     * Проверка верного постфикса.
      */
     @Test
-    public void whenStartWithPrefixThenTrue() {
+    public void whenEndWithPostfixThenTrue() {
         EndsWith word = new EndsWith();
         boolean result = word.endsWith("Hello", "lo");
         assertThat(result, is(true));
     }
 
     /**
-     * TODO add comments
+     * Проверка не верного постфикса.
      */
     @Test
-    public void whenNotStartWithPrefixThenFalse() {
+    public void whenNotEndWithPostfixThenFalse() {
         EndsWith word = new EndsWith();
         boolean result = word.endsWith("Hello", "la");
+        assertThat(result, is(false));
+    }
+
+    /**
+     * Проверка частного случая когда постфикс больше слова.
+     */
+    @Test
+    public void whenNotEndWithPostfixThenFalse2() {
+        EndsWith word = new EndsWith();
+        boolean result = word.endsWith("Hello", "HHello");
         assertThat(result, is(false));
     }
 }
