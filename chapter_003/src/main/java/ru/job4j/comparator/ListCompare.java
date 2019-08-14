8f6d72d4b90375da2cee156a3a8d5071640a158f
Evgeniy Novoselov <zkodru@gmail.com>
2019-08-14 14:27:53 +0300

    Подготовил методы и тесты для выполнения

---
+++ chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -0,0 +1,15 @@
+package ru.job4j.comparator;
+
+import java.util.Comparator;
+
+/**
+ * Компаратор для сравнения двух массивов символов.
+ *
+ * @author Evgeny Novoselov
+ */
+public class ListCompare implements Comparator<String> {
+    @Override
+    public int compare(String s, String t1) {
+        return 0;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/comparator/StringsCompareTest.java
@@ -0,0 +1,80 @@
+package ru.job4j.comparator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Тестирование компаратора для сравнения двух массивов символов.
+ *
+ * @author Evgeny Novoselov
+ */
+public class StringsCompareTest {
+    /**
+     * Проверка сравнения когда строки равны.
+     */
+    @Test
+    public void whenStringsAreEqualThenZero() {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanov"
+        );
+        assertThat(rst, is(0));
+    }
+
+    /**
+     * Проверка стравнения когда вторая строка больше.
+     */
+    @Test
+    public void whenLeftLessThanRightResultShouldBeNegative() {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanova"
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+    /**
+     * Проверка сравнения разных строк.
+     */
+    @Test
+    public void whenLeftGreaterThanRightResultShouldBePositive() {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Ivanova"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    /**
+     * Проверка сравнения различных слов.
+     */
+    @Test
+    public void secondCharOfLeftGreaterThanRightShouldBePositive() {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Patrov"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    /**
+     * Проверка сравнения различных слов.
+     */
+    @Test
+    public void secondCharOfLeftLessThanRightShouldBeNegative() {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Patrova",
+                "Petrov"
+        );
+        assertThat(rst, lessThan(0));
+    }
+}
