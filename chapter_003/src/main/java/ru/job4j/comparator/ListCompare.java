a92bf763d41765273d250b693c15d678a26ee5de
Evgeniy Novoselov <zkodru@gmail.com>
2019-08-14 14:57:30 +0300

    Написал метод сравнение строки

--- chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -10,6 +10,15 @@ import java.util.Comparator;
 public class ListCompare implements Comparator<String> {
     @Override
     public int compare(String s, String t1) {
-        return 0;
+        int result = 0;
+        for (int i = 0; i < s.length() - 1 || i < t1.length() - 1; i++) {
+            char cs = s.charAt(i);
+            char ct1 = t1.charAt(i);
+            if (cs != ct1) {
+                result = Character.compare(cs, ct1);
+                break;
+            }
+        }
+        return (result == 0 && s.length() != t1.length()) ? Integer.compare(s.length(), t1.length()) : result;
     }
 }
