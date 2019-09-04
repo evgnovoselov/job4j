c09a4f13b14dc879d31baeff2762389ee2e342f3
Evgeny Novoselov <zkodru@gmail.com>
2019-09-04 22:27:59 +0300

    Добавил в класс Account методы equals и hashCode

--- chapter_003/src/main/java/ru/job4j/bank/Account.java
+++ chapter_003/src/main/java/ru/job4j/bank/Account.java
@@ -1,5 +1,7 @@
 package ru.job4j.bank;
 
+import java.util.Objects;
+
 /**
  * Счет в банке.
  *
@@ -28,5 +30,18 @@ public class Account {
 
     public void setRequisites(String requisites) {
         this.requisites = requisites;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(requisites, account.requisites);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(requisites);
     }
 }
