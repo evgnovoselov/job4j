f7be85f74d31ba8bd3df1f6b5b63c8d9846d1bbf
Evgeny Novoselov <zkodru@gmail.com>
2019-09-04 21:39:32 +0300

    Добавил реализацию классов User и Account

--- chapter_003/src/main/java/ru/job4j/bank/User.java
+++ chapter_003/src/main/java/ru/job4j/bank/User.java
@@ -1,5 +1,7 @@
 package ru.job4j.bank;
 
+import java.util.Objects;
+
 /**
  * Клиенты банка.
  *
@@ -8,4 +10,39 @@ package ru.job4j.bank;
 public class User {
     private String name;
     private String passport;
+
+    public User(String name, String passport) {
+        this.name = name;
+        this.passport = passport;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) &&
+                Objects.equals(passport, user.passport);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, passport);
+    }
 }
