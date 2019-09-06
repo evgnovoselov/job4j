db80562eb8f195907546260d51b2f90bed7c36ba
Evgeniy Novoselov <zkodru@gmail.com>
2019-09-06 16:00:25 +0300

    Написал тест когда перевод денег невозможен между счетами разных пользователей

--- chapter_003/src/test/java/ru/job4j/bank/BankTest.java
+++ chapter_003/src/test/java/ru/job4j/bank/BankTest.java
@@ -135,5 +135,23 @@ public class BankTest {
      */
     @Test
     public void whenFromUserAccountTransferMoneyButHisNotHaveMoneyToAccountOtherUserThenAccountOtherUserDoNotHasMoney() {
+        Bank bank = new Bank();
+        bank.addUser(new User("Evgeny", "11"));
+        bank.addAccountToUser("11", new Account("1111"));
+        bank.getUserAccounts("11").get(0).setValue(300);
+        bank.addUser(new User("Petr", "22"));
+        bank.addAccountToUser("22", new Account("2222"));
+        assertThat(bank.transferMoney(
+                "11", "1111",
+                "22", "2222",
+                450), is(false)
+        );
+        assertThat(new Double[]{
+                bank.getUserAccounts("11").get(0).getValue(),
+                bank.getUserAccounts("22").get(0).getValue(),
+        }, is(new Double[]{
+                300.0,
+                0.0,
+        }));
     }
 }
