1793856cdc6e205844acaeb359fa540f7ea9a4ff
Evgeniy Novoselov <zkodru@gmail.com>
2019-09-06 13:59:36 +0300

    Написал тест удаления счета у пользователя

--- chapter_003/src/test/java/ru/job4j/bank/BankTest.java
+++ chapter_003/src/test/java/ru/job4j/bank/BankTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.nullValue;
@@ -40,5 +41,19 @@ public class BankTest {
         bank.addAccountToUser("11", new Account("2222"));
         bank.deleteUser(new User("Evgeny", "11"));
         assertThat(bank.getUserAccounts("11"), is(nullValue()));
+    }
+
+    /**
+     * Тестируем удаление счета у пользователя.
+     */
+    @Test
+    public void whenDeleteAccountFromUserThenBankDoNotHasIt() {
+        Bank bank = new Bank();
+        bank.addUser(new User("Evgeny", "11"));
+        bank.addAccountToUser("11", new Account("1111"));
+        bank.addAccountToUser("11", new Account("2222"));
+        bank.deleteAccountFromUser("11", new Account("2222"));
+        List<Account> expected = new ArrayList<>(Collections.singletonList(new Account("1111")));
+        assertThat(bank.getUserAccounts("11"), is(expected));
     }
 }
