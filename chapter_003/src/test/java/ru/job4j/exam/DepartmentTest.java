f10c220c658c1cfb734e2190db61c0c301d873d5
Evgeny Novoselov <zkodru@gmail.com>
2019-09-30 18:02:54 +0300

    Контрольные вопросы.[#104825] (#22)
    Задача 34352

---
+++ chapter_003/src/test/java/ru/job4j/exam/DepartmentTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.exam;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тестирования сортировки департаментов.
+ *
+ * @author Evgeny Novoselov
+ */
+public class DepartmentTest {
+    /**
+     * Сортируем департаменты в порядке возростания.
+     */
+    @Test
+    public void whenDepartmentsSortThenGetDepartmentsSort() {
+        String[] departments = {
+                "K1\\SK1",
+                "K1\\SK2",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K2",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1\\SSK2"
+        };
+        String[] expected = {
+                "K1",
+                "K1\\SK1",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K1\\SK2",
+                "K2",
+                "K2\\SK1",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1\\SSK2"
+        };
+        String[] departmentsSortAsc = Department.sort(departments);
+        assertThat(departmentsSortAsc, is(expected));
+    }
+
+    /**
+     * Сортируем департаменты в порядке убывания.
+     */
+    @Test
+    public void whenDepartmentsReversSortThenDepartmentsReverseSort() {
+        String[] departments = {
+                "K1\\SK1",
+                "K1\\SK2",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K2",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1\\SSK2"
+        };
+        String[] expected = {
+                "K2",
+                "K2\\SK1",
+                "K2\\SK1\\SSK2",
+                "K2\\SK1\\SSK1",
+                "K1",
+                "K1\\SK2",
+                "K1\\SK1",
+                "K1\\SK1\\SSK2",
+                "K1\\SK1\\SSK1",
+        };
+        String[] departmentsSortDesc = Department.reverseSort(departments);
+        assertThat(departmentsSortDesc, is(expected));
+    }
+}
