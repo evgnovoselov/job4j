ec745a89b84162f6bec146a0f684c4f37826d857
Evgeny Novoselov <zkodru@gmail.com>
2019-10-05 15:29:58 +0300

    Реализовал первый метод

--- chapter_004/src/test/java/ru/job4j/lambda/DiapasonTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/DiapasonTest.java
@@ -1,9 +1,24 @@
 package ru.job4j.lambda;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * TODO Add comments.
  *
  * @author Evgeny Novoselov
  */
 public class DiapasonTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        Diapason function = new Diapason();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
 }
