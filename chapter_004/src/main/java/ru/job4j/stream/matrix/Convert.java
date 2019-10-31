0dafc1013e9f804662e55d2a6e4655cbb0d2c5f8
Evgeniy Novoselov <zkodru@gmail.com>
2019-10-31 16:15:27 +0300

    Подготовил классы для решения задачи

---
+++ chapter_004/src/main/java/ru/job4j/stream/matrix/Convert.java
@@ -0,0 +1,20 @@
+package ru.job4j.stream.matrix;
+
+import java.util.List;
+
+/**
+ * Класс преобразования.
+ *
+ * @author Evgeny Novoselov
+ */
+public class Convert {
+    /**
+     * Метод преобразования матрицы чисел в список чисел.
+     *
+     * @param integers матрица чисел.
+     * @return список чисел.
+     */
+    public static List<Integer> toList(Integer[][] integers) {
+        return null;
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/stream/matrix/ConvertTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.stream.matrix;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тестирование класса преобразования.
+ *
+ * @author Evgeny Novoselov
+ */
+public class ConvertTest {
+    /**
+     * Тестирование преобразование из матрицы чисел в список чисел.
+     */
+    @Test
+    public void whenIntegerMatrixConvertToIntegerListThenIntegerList() {
+        assertThat(false, is(true));
+    }
+}
