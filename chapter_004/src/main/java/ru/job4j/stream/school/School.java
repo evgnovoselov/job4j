71c72593291e25072c9b53ab53df66af8fee7ce1
Evgeniy Novoselov <zkodru@gmail.com>
2019-10-31 15:42:07 +0300

    Реализовал метод конвертации списка учеников в карту

--- chapter_004/src/main/java/ru/job4j/stream/school/School.java
+++ chapter_004/src/main/java/ru/job4j/stream/school/School.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream.school;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -21,5 +22,20 @@ public class School {
         return students.stream().filter(
                 predict
         ).collect(Collectors.toList());
+    }
+
+    /**
+     * Преобразование списка учеников в карту их имен и объектов учеников.
+     *
+     * @param students список учеников.
+     * @return Карта учеников с их фамилиями на их объекты.
+     */
+    public static Map<String, Student> toMap(List<Student> students) {
+        return students.stream().distinct().collect(
+                Collectors.toMap(
+                        Student::getSurname,
+                        student -> student
+                )
+        );
     }
 }
--- chapter_004/src/test/java/ru/job4j/stream/school/SchoolTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/school/SchoolTest.java
@@ -3,7 +3,9 @@ package ru.job4j.stream.school;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -27,9 +29,9 @@ public class SchoolTest {
                 new Student(100)
         );
         List<Student> expected = Arrays.asList(
-                new Student(70),
-                new Student(75),
-                new Student(100)
+                students.get(1),
+                students.get(3),
+                students.get(4)
         );
         List<Student> result = School.collect(students, student -> 70 <= student.getScore() && student.getScore() <= 100);
         assertThat(result, is(expected));
@@ -48,8 +50,8 @@ public class SchoolTest {
                 new Student(100)
         );
         List<Student> expected = Arrays.asList(
-                new Student(50),
-                new Student(69)
+                students.get(0),
+                students.get(2)
         );
         List<Student> result = School.collect(students, student -> 50 <= student.getScore() && student.getScore() < 70);
         assertThat(result, is(expected));
@@ -69,10 +71,29 @@ public class SchoolTest {
                 new Student(100)
         );
         List<Student> expected = Arrays.asList(
-                new Student(30),
-                new Student(1)
+                students.get(0),
+                students.get(3)
         );
         List<Student> result = School.collect(students, student -> 0 < student.getScore() && student.getScore() < 50);
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Тестирование преобразования списка учеников в Map.
+     */
+    @Test
+    public void whenStudentListConvertToMapThenStudentMap() {
+        List<Student> students = Arrays.asList(
+                new Student("Surname1", 30),
+                new Student("Surname2", 70),
+                new Student("Surname3", 69),
+                new Student("Surname4", 1)
+        );
+        Map<String, Student> expected = new HashMap<>();
+        for (Student student : students) {
+            expected.put(student.getSurname(), student);
+        }
+        Map<String, Student> result = School.toMap(students);
         assertThat(result, is(expected));
     }
 }
