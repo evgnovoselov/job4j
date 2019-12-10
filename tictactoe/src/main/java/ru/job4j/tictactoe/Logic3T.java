6cd7ce669f23c259d4a9107d2287a57ceb735f6f
Evgeniy Novoselov <zkodru@gmail.com>
2019-12-10 13:58:49 +0300

    Сделал без проверки диагонали

--- tictactoe/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ tictactoe/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -10,13 +10,29 @@ public class Logic3T {
     }
 
     public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
-        boolean result = true;
-        for (int index = 0; index != this.table.length; index++) {
-            Figure3T cell = this.table[startX][startY];
-            startX += deltaX;
-            startY += deltaY;
-            if (!predicate.test(cell)) {
-                result = false;
+        boolean result = false;
+        for (int row = 0; row < this.table.length; row++) {
+            int countCol = 0;
+            for (int col = 0; col < this.table.length; col++) {
+                int countRow = 0;
+                if (predicate.test(this.table[row][col])) {
+                    countCol++;
+                }
+                if (row == 0) {
+                    for (row = 0; row < this.table.length; row++) {
+                        if (predicate.test(this.table[row][col])) {
+                            countRow++;
+                        }
+                    }
+                    row = 0;
+                    if (countRow == this.table.length) {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+            if (result || countCol == this.table.length) {
+                result = true;
                 break;
             }
         }
@@ -31,8 +47,7 @@ public class Logic3T {
                 || this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1)
                 || this.fillBy(Figure3T::hasMarkX, 2, 0, 0, 1)
                 || this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0)
-                || this.fillBy(Figure3T::hasMarkX, 0, 2, 1, 0)
-                ;
+                || this.fillBy(Figure3T::hasMarkX, 0, 2, 1, 0);
     }
 
     public boolean isWinnerO() {
