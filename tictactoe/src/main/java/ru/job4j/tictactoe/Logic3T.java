2a475777127bef925786a6a432ad93411cac78cf
Evgeny Novoselov <zkodru@gmail.com>
2019-12-11 11:20:00 +0300

    Крестики-нолики на JavaFX[#104743] (#41)

---
+++ tictactoe/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -0,0 +1,50 @@
+package ru.job4j.tictactoe;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class Logic3T {
+    private final Figure3T[][] table;
+
+    public Logic3T(Figure3T[][] table) {
+        this.table = table;
+    }
+
+    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
+        boolean result = true;
+        for (int index = 0; index != this.table.length; index++) {
+            Figure3T cell = this.table[startX][startY];
+            startX += deltaX;
+            startY += deltaY;
+            if (!predicate.test(cell)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private boolean isWinner(Predicate<Figure3T> predicate) {
+        return this.fillBy(predicate, 0, 0, 1, 0)
+                || this.fillBy(predicate, 0, 0, 0, 1)
+                || this.fillBy(predicate, 0, 0, 1, 1)
+                || this.fillBy(predicate, this.table.length - 1, 0, -1, 1)
+                || this.fillBy(predicate, 1, 0, 0, 1)
+                || this.fillBy(predicate, 2, 0, 0, 1)
+                || this.fillBy(predicate, 0, 1, 1, 0)
+                || this.fillBy(predicate, 0, 2, 1, 0);
+    }
+
+    public boolean isWinnerX() {
+        return this.isWinner(Figure3T::hasMarkX);
+    }
+
+    public boolean isWinnerO() {
+        return this.isWinner(Figure3T::hasMarkO);
+    }
+
+    public boolean hasGap() {
+        return Arrays.stream(this.table).flatMap(Stream::of).anyMatch(e -> !e.hasMarkO() && !e.hasMarkX());
+    }
+}
